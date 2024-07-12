0063eadfe3ce626ac6f86ec084008d17a588939d
aptylertsai <129021886+aptylertsai@users.noreply.github.com>
2024-07-12 15:39:18 +0800

    fix: add devault value

--- sql/202407121425_create_table_t5.sql
+++ sql/202407121425_create_table_t5.sql
@@ -1,7 +1,7 @@
 CREATE TABLE public.test_table(
     id INT PRIMARY KEY,
-    created_ts TIMESTAMP NOT NULL,
+    created_ts TIMESTAMP NOT NULL DEFAULT NOW,
     creator_id INT NOT NULL,
-    updated_ts TIMESTAMP NOT NULL,
+    updated_ts TIMESTAMP NOT NULL DEFAULT NOW,
     updater_id INT NOT NULL
 )
